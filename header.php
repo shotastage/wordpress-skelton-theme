6d3fe8b1edfcad1a19d83b591064dacbbf15fcbe
Shota Shimazu <shotastage@users.noreply.github.com>
2019-10-20 18:04:36 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="utf-8">
+        <title><?php wp_title(' | ', true, 'right'); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
